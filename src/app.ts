67dfb518d588c4fc51d500b854e34fa88c01b8ce
Jose-Murilo <murilojucas360@hotmail.com>
2024-02-01 15:44:52 -0300

    chore: separate app of server

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import fastify from "fastify";
 import { tasksRoutes } from "./routes/tasks.routes";
+import { usersRoutes } from "./routes/users.routes";
+import cookie from "@fastify/cookie";
 
 export const app = fastify()
+
+app.register(cookie)
+
+app.register(usersRoutes, {
+  prefix: "/users"
+})
 
 app.register(tasksRoutes, {
   prefix: "/tasks"
