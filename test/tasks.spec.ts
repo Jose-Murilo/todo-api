2327b1cc91ef35fdb6759b0d803e3aec7200b9ad
Jose-Murilo <murilojucas360@hotmail.com>
2024-02-10 23:48:24 -0300

    test: test in tasks preparate to test

--- test/tasks.spec.ts
+++ test/tasks.spec.ts
@@ -1,8 +1,28 @@
 import { describe, it, beforeAll, beforeEach, afterAll } from 'vitest'
 import { execSync } from 'node:child_process'
 import request from 'supertest'
+import { app } from '../src/app'
+
+// app.get('/', { preHandler: [checkSessionIdExists] }, async (request, reply) => {
+//   const listAllTasks = await knex('tasks').select()
+
+//   return { tasks: listAllTasks }
+// })
 
 describe('Tasks routes', async () => {
+  beforeAll(() => {
+    app.ready()  
+  })
+
+  afterAll(() => {
+    app.close()  
+  })
+
+  beforeEach(() => {
+    execSync("npm run knex migrate:rollback --all")
+    execSync("npm run knex migrate:latest")
+  })
+
   it('should be albe to a ', () => {
     
   })
