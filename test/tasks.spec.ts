b1a17d2ceabcb1a2b058f99c2ef837348f81e552
Jose-Murilo <murilojucas360@hotmail.com>
2024-02-10 23:45:42 -0300

    test: initialization test of tasks

---
+++ test/tasks.spec.ts
@@ -0,0 +1,9 @@
+import { describe, it, beforeAll, beforeEach, afterAll } from 'vitest'
+import { execSync } from 'node:child_process'
+import request from 'supertest'
+
+describe('Tasks routes', async () => {
+  it('should be albe to a ', () => {
+    
+  })
+})
